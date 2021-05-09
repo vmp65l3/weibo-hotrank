552fd9a163bcae726fddf5a2c3943bd4ad27ed16
暗号 <626522796@qq.com>
2021-05-09 18:42:56 +0800

    add update-radme.py

---
+++ update-readme.py
@@ -0,0 +1,4 @@
+import datetime
+today = datetime.date.today()
+f = open('README.md', 'a')
+f.write('##' + str(today) + '\n' + '![image text](https://github.com/vmp65l3/weibo-hotrank/blob/master/' + str(today) + '.jpg)')
