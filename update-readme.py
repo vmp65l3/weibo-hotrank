5186bbad5aa19b7e18bc47660af362c0a468b86f
暗号 <626522796@qq.com>
2021-05-10 16:11:37 +0800

    整理代码

--- update-readme.py
+++ update-readme.py
@@ -1,12 +1,8 @@
 from datetime import datetime, timedelta
 
 utctime = datetime.utcnow()
-
 bjtime = utctime + timedelta(hours=8)
-
 bjtime = bjtime.strftime('%Y-%m-%d')
-
-
 
 f = open('README.md', 'a')
 f.write('\n' + '##' + ' ' + str(bjtime) + '\n' + '![image text](https://github.com/vmp65l3/weibo-hotrank/blob/master/fig/' + str(bjtime) + '.jpg)' + '\n')
